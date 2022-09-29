59c88a2683dfcd51738b81e58b7ca0c4b355dd6a
vishal <104211252+vishal07vishal@users.noreply.github.com>
2022-09-29 12:14:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { Form } from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+       <h1 className="heading">EMPLOYEE FEEDBACK FORM</h1>
+      <Form/>
+      
+    </div>
+  );
+}
+
+export default App;
